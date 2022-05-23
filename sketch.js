2a9f6ae2a6f81a0b9b74c4454883ada3306ff6db
SalmaPZ <47847444+SalmaPZ@users.noreply.github.com>
2022-05-23 16:59:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,15 +26,8 @@ function preload()
   bg_img = loadImage('background.png');
   food = loadImage('melon.png');
   rabbit = loadImage('Rabbit-01.png');;
-  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
-  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
-  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
   
-  blink.playing = true;
-  eat.playing = true;
-  sad.playing = true;
-  sad.looping= false;
-  eat.looping = false; 
+
 }
 
 function setup() {
@@ -50,15 +43,11 @@ function setup() {
   button.mouseClicked(drop);
   
 
-  blink.frameDelay = 20;
-  eat.frameDelay = 20;
+  
   bunny = createSprite(230,620,100,100);
   bunny.scale = 0.2;
-
-  bunny.addAnimation('blinking',blink);
-  bunny.addAnimation('eating',eat);
-  bunny.addAnimation('crying',sad);
-  bunny.changeAnimation('blinking');
+  bunny.addImage(rabbit);
+  
   
   rope = new Rope(7,{x:245,y:30});
   ground = new Ground(200,690,600,20);
